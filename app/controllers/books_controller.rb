e5f5e6f02d8443b5219500b8acb33e43b110c77d
mardyz <107136761+mardyz@users.noreply.github.com>
2022-06-28 17:34:22 +0000

    [Update]showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,25 @@
 class BooksController < ApplicationController
   def new
+    @books = Books.new
+  end
+
+  def create
+    @books = Books.new(books_params)
+    @books.save
+    redirect_to 'show'
   end
 
   def index
-    @books = Books
   end
 
   def show
   end
 
   def edit
+  end
+
+private
+  def books_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   get '/' => 'homes#top'
+  post 'books' => 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
