4e18c3a792b92e11d94e8e1196473113d0ff22f7
mardyz <107136761+mardyz@users.noreply.github.com>
2022-07-03 16:03:19 +0000

    [Update]indexページレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
   def new
-    @books = Books.new
+    @book = Book.new
   end
 
   def create
-    @books = Books.new(books_params)
-    @books.save
-    redirect_to 'show'
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -18,8 +20,11 @@ class BooksController < ApplicationController
   def edit
   end
 
-private
-  def books_params
+  def destroy
+  end
+
+ private
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
