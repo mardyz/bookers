a5ddaa1c7df3e861af898a76c421de192657f129
mardyz <107136761+mardyz@users.noreply.github.com>
2022-07-04 15:28:42 +0000

    [finish]エラー確認後

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash[:alret] = "Book was unsuccessfully created."
-      render "books"
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,18 +25,26 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to "/books"
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to "/books"
+    end
   end
 
  private
