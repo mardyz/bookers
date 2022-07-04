6b0f26bad61c678c713dadc2c4be07c3b1cbb8c5
mardyz <107136761+mardyz@users.noreply.github.com>
2022-07-04 00:28:13 +0000

    [Update]editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,9 +19,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy(book_params)
+    redirect_to "/book"
   end
 
  private
